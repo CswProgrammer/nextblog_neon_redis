b6aa2f601ab4a9e5c7dcae2a3fa24ad843c290e7
CswProgrammer <113418245+CswProgrammer@users.noreply.github.com>
2025-08-02 11:13:39 +0800

    Update app.ts

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,6 +1,6 @@
 // src/config/app.ts
 import type { AppConfig } from "@/libs/types";
 export const appConfig: AppConfig = {
-  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://nextblog-neon-stylelintfix-lzvp.vercel.app",,
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL || "https://nextblog-neon-stylelintfix-lzvp.vercel.app",
   apiUrl: process.env.NEXT_PUBLIC_API_URL || `${process.env.NEXT_PUBLIC_BASE_URL}/api`,
 };
