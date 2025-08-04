9f3a4e54d7a93af4a2c594fc103f6a6c0fa5f28e
CswProgrammer <113418245+CswProgrammer@users.noreply.github.com>
2025-08-04 10:25:16 +0800

    Update layout.tsx

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,6 +1,7 @@
 // src/app/(pages)/layout.tsx
 import type { Metadata } from "next";
 import type { FC, PropsWithChildren, ReactNode } from "react";
+import Head from "next/head"; // 导入 Head 组件
 
 import { Auth } from "../_components/auth/provider";
 import { Header } from "../_components/header";
@@ -8,6 +9,7 @@ import "./global.css";
 import { Toaster } from "../_components/shadcn/ui/toaster";
 import Theme from "../_components/theme";
 import $styles from "./layout.module.css";
+
 export const metadata: Metadata = {
   title: "编程突围",
   description: "这里是编程突围官网",
@@ -16,6 +18,14 @@ export const metadata: Metadata = {
 const AppLayout: FC<PropsWithChildren<{ modal: ReactNode }>> = ({ children, modal }) => (
   <Auth>
     <Theme>
+      {/* 添加 viewport meta 标签 */}
+      <Head>
+        <meta 
+          name="viewport" 
+          content="width=device-width, initial-scale=1, viewport-fit=cover" 
+        />
+      </Head>
+      
       <div className={$styles.layout}>
         <Header />
         {children}
@@ -25,4 +35,5 @@ const AppLayout: FC<PropsWithChildren<{ modal: ReactNode }>> = ({ children, moda
     </Theme>
   </Auth>
 );
+
 export default AppLayout;
